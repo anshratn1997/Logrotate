211243933fd026951d3ab97b4e324a381133a9a6
ratneshkeshari <ratnesh.keshari@box8.in>
2020-02-16 17:19:11 +0530

    change configuration

--- logrotate.sh
+++ logrotate.sh
@@ -3,13 +3,14 @@
 # output_app.log
 set -e
 
-HOME_DIR=$1
-CURRENT_LOG_FILE="$HOME_DIR/$2"
-PREVIOUS_DAY_LOG_FILE="$HOME_DIR/$3"
-OLDEST_FILE="$HOME_DIR/$4"
+CURRENT_LOG_FILE=$1
+HOME_DIR="${CURRENT_LOG_FILE%/*}"
+PREVIOUS_DAY_LOG_FILE="$CURRENT_LOG_FILE.1"
+OLDEST_FILE="$CURRENT_LOG_FILE.$2.gz"
 
 
-# OLDEST_FILE=output_app.log.5.gz
+# OLDEST_FILE="$CURRENT_LOG_FILE_NAME.$2.gz"
+
 if [ -f $OLDEST_FILE ]; then
     rm $OLDEST_FILE
 else 
@@ -21,19 +22,26 @@ for file in $(find $HOME_DIR -name '*.gz'  -mindepth 1 -maxdepth 1 | sort -rn); 
     second_ex="${file_name_without_ex##*.}"
     file_name_without_second_ex="${file_name_without_ex%.*}"
     incremented_second_ex=$(echo "$second_ex" | tr "0-9a-z" "1-9a-z_")
-    new_file_name="$file_name_without_second_ex.$incremented_second_ex.gz"
-    mv $file $new_file_name
+    
+    if [ $incremented_second_ex -gt $2 ]; then
+        rm $file
+    else
+        new_file_name="$file_name_without_second_ex.$incremented_second_ex.gz"
+        mv $file $new_file_name
+    fi
+    
 done
 
 
-# PREVIOUS_DAY_LOG_FILE=output_app.log.1
+# PREVIOUS_DAY_LOG_FILE="$CURRENT_LOG_FILE.1"
+
 if [ -f $PREVIOUS_DAY_LOG_FILE ]; then
     gzip $PREVIOUS_DAY_LOG_FILE
 else
     echo "$PREVIOUS_DAY_LOG_FILE does not exist"
 fi
 
-# CURRENT_LOG_FILE=output_app.log
+
 if [ -f $CURRENT_LOG_FILE ]; then
     mv $CURRENT_LOG_FILE "$CURRENT_LOG_FILE.1"
 else
